99680242ee3605bbf1a8a56b5ff4742714dd1e7f
Ankitagole <ankita17@navgurukul.org>
2019-05-28 12:55:31 +0530

    initial commit

---
+++ test/java/Game/RockPaperScissorTest.java
@@ -0,0 +1,20 @@
+package Game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RockPaperScissorTest {
+    @Test public void itShouldGiveTheGameResultDrawWhenBothTheGuessIsSame(){
+        String expected = "draw";
+        assertEquals(expected,RockPaperScissorsGame.getUserGuess("paper","paper"));
+    }
+    @Test public void itShouldGiveTheGameResultYouLoose(){
+        String expected = "you loose";
+        assertEquals(expected,RockPaperScissorsGame.getUserGuess("paper","rock"));
+    }
+    @Test public void itShouldGiveTheGameResultYouWin(){
+        String expected = "you win";
+        assertEquals(expected,RockPaperScissorsGame.getUserGuess("scissor","rock"));
+    }
+}
