fc5579aa5194c329805cd7a22443c1ad0b0e06cb
Ankitagole <ankita17@navgurukul.org>
2019-05-28 12:33:57 +0530

    refactor in function

--- RockPaperScissorsGame.java
+++ RockPaperScissorsGame.java
@@ -9,28 +9,36 @@ public class RockPaperScissorsGame {
             System.out.println("What do you pick? (rock, paper, scissors)");
             String userGuess = input.nextLine().toLowerCase();
             List<String> moves = Arrays.asList("paper", "scissor", "rock");
-            Random rand = new Random();
-            String computerGuess = moves.get(rand.nextInt(moves.size()));
-
             if (!moves.contains(userGuess)) {
                 System.out.println("invalid input");
             }
-            if (computerGuess.equals(userGuess)) {
-                System.out.println("draw");
-            } else if (userGuess.equals("paper") && computerGuess.equals("rock")) {
-                System.out.println("you win");
-            } else if (userGuess.equals("rock") && computerGuess.equals("scissor")) {
-                System.out.println("you win");
-            } else if (userGuess.equals("scissor") && computerGuess.equals("paper")) {
-                System.out.println("you win");
-            } else {
-                System.out.println("you loose");
+            else {
+                System.out.println(getUserGuess(userGuess, getComputerGuess(moves)));
             }
-            System.out.println("for play again yes and no");
+            System.out.println("for play again (yes and no)");
             String again = input.nextLine().toLowerCase();
             if (again.equals("no")){
                 break;
             }
+        }
+    }
+    public static String getComputerGuess(List<String> moves){
+        Random rand = new Random();
+        return moves.get(rand.nextInt(moves.size()));
+    }
+
+    public static String getUserGuess(String computerGuess, String userGuess){
+        if (computerGuess.equals(userGuess)) {
+            return "draw";
+        } else if (userGuess.equals("paper") && computerGuess.equals("rock")) {
+            return "you win";
+        } else if (userGuess.equals("rock") && computerGuess.equals("scissor")) {
+            return "you win";
+        } else if (userGuess.equals("scissor") && computerGuess.equals("paper")) {
+            return "you win";
+        }
+        else {
+            return "you loose";
         }
     }
 }
