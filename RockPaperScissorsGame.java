521bb44c7bce60f0519707b0b4bc567c2138cb32
Ankitagole <ankita17@navgurukul.org>
2019-05-28 12:00:04 +0530

    add. play again feature

--- RockPaperScissorsGame.java
+++ RockPaperScissorsGame.java
@@ -4,29 +4,35 @@ import java.util.Random;
 import java.util.Scanner;
 public class RockPaperScissorsGame {
     public static void main(String[] args) {
-        List<String> moves = Arrays.asList("paper", "scissor", "rock");
-        Scanner input = new Scanner(System.in);
-        System.out.println("What do you pick? (rock, paper, scissors)");
-        String userGuess = input.nextLine().toLowerCase();
-        Random rand = new Random();
-        String computerGuess = moves.get(rand.nextInt(moves.size()));
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What do you pick? (rock, paper, scissors)");
+            String userGuess = input.nextLine().toLowerCase();
+            List<String> moves = Arrays.asList("paper", "scissor", "rock");
+            Random rand = new Random();
+            String computerGuess = moves.get(rand.nextInt(moves.size()));
 
-        if(!moves.contains(userGuess)) {
-            System.out.println("invalid input");
-        }
-        else if(computerGuess.equals(userGuess)){
-            System.out.println("draw");
-        }
-        else if(userGuess.equals("paper") && computerGuess.equals("rock")){
-            System.out.println("you win");
-        }
-
-        else if(userGuess.equals("rock") && computerGuess.equals("scissor")){
-            System.out.println("you win");
-        }
-
-        else if(userGuess.equals("scissor") && computerGuess.equals("paper")){
-            System.out.println("you win");
+            if (!moves.contains(userGuess)) {
+                System.out.println("invalid input");
+            }
+            if (computerGuess.equals(userGuess)) {
+                System.out.println("draw");
+            } else if (userGuess.equals("paper") && computerGuess.equals("rock")) {
+                System.out.println("you win");
+            } else if (userGuess.equals("rock") && computerGuess.equals("scissor")) {
+                System.out.println("you win");
+            } else if (userGuess.equals("scissor") && computerGuess.equals("paper")) {
+                System.out.println("you win");
+            } else {
+                System.out.println("you loose");
+            }
+            System.out.println("for play again yes and no");
+            String again = input.nextLine().toLowerCase();
+            if (again.equals("no")){
+                break;
+            }
         }
     }
 }
+
+
